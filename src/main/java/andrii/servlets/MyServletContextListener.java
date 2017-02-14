9be36e4055bc7a0a4cc9590cae3a8a40ad8cb547
andriyostryzhniuk <ostryzhnykandriy@gmail.com>
2017-02-14 18:52:37 +0200

    MyServletContextListener created

---
+++ src/main/java/andrii/servlets/MyServletContextListener.java
@@ -0,0 +1,20 @@
+package andrii.servlets;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Created by Andriy on 02/14/2017.
+ */
+public class MyServletContextListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+}

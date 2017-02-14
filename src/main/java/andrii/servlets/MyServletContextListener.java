7ad0a6206e3a3b90d9223b7ae7a2dac15951da95
andriyostryzhniuk <ostryzhnykandriy@gmail.com>
2017-02-14 19:37:49 +0200

    MyServletContextListener with dog's breed finished

--- src/main/java/andrii/servlets/MyServletContextListener.java
+++ src/main/java/andrii/servlets/MyServletContextListener.java
@@ -1,5 +1,8 @@
 package andrii.servlets;
 
+import andrii.model.Dog;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -11,6 +14,13 @@ public class MyServletContextListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
 
+        ServletContext servletContext = servletContextEvent.getServletContext();
+
+        String dogBreed = servletContext.getInitParameter("breed");
+
+        Dog dog = new Dog(dogBreed);
+
+        servletContext.setAttribute("dog", dog);
     }
 
     @Override
